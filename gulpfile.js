c28bc786ee78951650192649e965ede537888f27
Riccardo Bellingeri <rbellingeri@gilt.com>
2017-09-18 11:27:49 +0100

    chore: watches html file and reloads browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,7 @@ gulp.task('browser-sync', () => {
   gulp.watch('partials/*.md').on('change', () => {
     execa.shell('npm run concat').then(browserSync.reload)
   });
+  gulp.watch('docs/*.html').on('change', browserSync.reload);
 })
 
 gulp.task('default', ['browser-sync'])
